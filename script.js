16a31f1132c788054728d7a418edc8f90ac4dc65
CodeCapitainess <masailimedveda73@gmail.com>
2024-03-04 04:56:02 +0400

    Restaurant_website

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener("load", function () {
  * add event listener on multiple elements
 */
 
-const addEventListener = function (elements, eventType, callBack) {
+const addEventOnElements = function (elements, eventType, callBack) {
     for (let i = 0, len = elements.length; i < len; i++){
         elements[i].addEventListener(eventType, callBack);
     }
@@ -29,4 +29,20 @@ const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = function () {}
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+/** Header */
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+}
