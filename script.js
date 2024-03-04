7e6423417ed748e3c4153266086c9ef7f2e83b73
CodeCapitainess <masailimedveda73@gmail.com>
2024-03-04 04:51:22 +0400

    Restaurant_website

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ window.addEventListener("load", function () {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
+
+
+/**
+ * add event listener on multiple elements
+*/
+
+const addEventListener = function (elements, eventType, callBack) {
+    for (let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callBack);
+    }
+}
+
+
+/** Navbar */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {}
